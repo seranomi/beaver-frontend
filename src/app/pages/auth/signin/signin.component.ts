518b55f1beeec7cf47e1838dccea88ad05e079e5
seranomi <2bumchan@naver.com>
2025-02-14 17:00:53 +0900

    feat: 로그인 기능 구현

---
+++ src/app/pages/auth/signin/signin.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss'],
+  standalone: false,
+})
+export class SigninComponent implements OnInit {
+  email: string = '';
+  password: string = '';
+
+  constructor(
+    private authService: AuthService, 
+    private router: Router
+  ) {}
+
+  ngOnInit() {}
+
+  async onSignIn() {
+    const signInData = {
+      email: this.email,
+      password: this.password,
+    };
+    console.log('signInData:', signInData);
+
+    this.authService.signIn(signInData).subscribe({
+      next: response => {
+        if (response.success) {
+          this.router.navigate(['/']);
+        } else {
+          console.error('Sign In failed:', response.message);
+        }
+      },
+      error: err => {
+        console.error('Sign In error:', err);
+      },
+      complete: () => {
+        console.log('Sign In request completed.');
+      }
+    });
+  }
+}
