11b2e67bece80e72f544fe63755c5ff7cdef78ce
seranomi <2bumchan@naver.com>
2025-02-14 17:00:40 +0900

    feat: 회원가입 기능 구현

---
+++ src/app/models/auth/sign-up-request.interface.ts
@@ -0,0 +1,10 @@
+import { UserRole } from "./user-role.enum";
+
+
+export interface SignUpRequest {
+    email: string;
+    username: string;
+    password: string;
+    passwordConfirm: string;
+    role: UserRole;
+}
---
+++ src/app/pages/auth/signup/signup.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { UserRole } from 'src/app/models/auth/user-role.enum';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+  standalone: false,
+})
+export class SignupComponent implements OnInit {
+  email: string = '';
+  username: string = '';
+  password: string = '';
+  passwordConfirm: string = '';
+  role: UserRole = UserRole.USER;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {}
+
+  async onSignUp() {
+    const signUpData = {
+      email: this.email,
+      username: this.username,
+      password: this.password,
+      passwordConfirm: this.passwordConfirm,
+      role: this.role,
+    };
+    
+    this.authService.signUp(signUpData).subscribe({
+      next: response => {
+        if (response.success) {
+          this.router.navigate(['/auth/signin']);
+        } else {
+          console.error('Sign Up failed:', response.message);
+        }
+      },
+      error: err => {
+        console.error('Sign Up error:', err);
+      },
+      complete: () => {
+        console.log('Sign Up request completed.');
+      }
+    });
+  }
+}
