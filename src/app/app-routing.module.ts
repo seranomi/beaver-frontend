d225f61a33d34b3043eb0fd142e8a90c2a10c13b
seranomi <2bumchan@naver.com>
2025-02-14 16:53:12 +0900

    chore: 프로젝트 초기 구조 생성

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'articles',
+    loadChildren: () => import('./pages/articles/articles.module').then( m => m.ArticlesModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./pages/auth/auth.module').then( m => m.AuthModule)
   },
 ];
 
